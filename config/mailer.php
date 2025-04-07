0f0baf3c444493da47ab594b30fc0e668fb0faca
Gilson Reis <gilsonc.reis@gmail.com>
2025-04-07 14:55:53 -0300

    ajuste em SMTP

--- config/mailer.php
+++ config/mailer.php
@@ -9,10 +9,12 @@ return [
     'transport' => [
 //                'dsn' => 'smtp://no-reply@simplifysoftwares.com.br:ie2jZ4E5@smtp.simplifysoftwares.com.br.com:587',
         'dsn' => sprintf(
-            '%s://%s:%s@default?verify_peer=0',
+            '%s://%s:%s@%s:%s',
             $_ENV['SMTP_ENCRYPTION'], // smtp, smtp+tls, smtp+ssl
             $_ENV['SMTP_USERNAME'],
-            $_ENV['SMTP_PASSWORD']
+            $_ENV['SMTP_PASSWORD'],
+            $_ENV['SMTP_HOST'],
+            $_ENV['SMTP_PORT']
         ),
     ],
 ];
